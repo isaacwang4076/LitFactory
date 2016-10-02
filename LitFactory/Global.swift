e1c902bc50f919652f998d37c5cffe645b9480bb
Isaac Wang <isaacwang4076@gmail.com>
2016-10-01 17:01:10 -0700

    Added User.swift, Globals.swft, and Podfile for Firebase

---
+++ LitFactory/Global.swift
@@ -0,0 +1,13 @@
+//
+//  Global.swift
+//  LitFactory
+//
+//  Created by Isaac Wang on 10/1/16.
+//  Copyright © 2016 DankMemrs. All rights reserved.
+//
+
+import Foundation
+
+struct Globals {
+    
+}
